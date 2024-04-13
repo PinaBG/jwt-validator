0b005c84b68849cf07a115fc8c3f280573eecf47
PinaBG <brunogabrielpina@gmail.com>
2024-04-13 17:51:02 -0300

    feat: cria spring application

---
+++ src/test/java/br/com/pinabg/jwtvalidator/JwtvalidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pinabg.jwtvalidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtvalidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
