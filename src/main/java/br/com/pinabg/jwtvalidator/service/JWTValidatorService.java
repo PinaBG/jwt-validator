819892ab16244e50a838256631b1fb0eace08414
PinaBG <brunogabrielpina@gmail.com>
2024-04-14 17:46:58 -0300

    feat: implementa metodo verificador de numeros na claim nome

--- src/main/java/br/com/pinabg/jwtvalidator/service/JWTValidatorService.java
+++ src/main/java/br/com/pinabg/jwtvalidator/service/JWTValidatorService.java
@@ -1,10 +1,24 @@
 package br.com.pinabg.jwtvalidator.service;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class JWTValidatorService {
 	public boolean stringIsNotNullOrEmpty(String name) {
 	    return !(name == null || name.isBlank());
+	}
+	
+	public boolean nameDontContainsNumbers(String name) {
+		boolean isValid = true;
+		
+		Pattern pattern = Pattern.compile("\\d+");
+		Matcher matcher = pattern.matcher(name);
+		if(matcher.find()) {
+			isValid = false;
+		}
+		return isValid;
 	}
 }
