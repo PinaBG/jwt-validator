dd0b21d5b89f6c0e870aa5f80307f0ce903ed3d7
PinaBG <brunogabrielpina@gmail.com>
2024-04-13 23:25:35 -0300

    feat: cria classe service

--- src/main/java/br/com/pinabg/jwtvalidator/controller/JWTValidatorController.java
+++ src/main/java/br/com/pinabg/jwtvalidator/controller/JWTValidatorController.java
@@ -1,11 +1,18 @@
 package br.com.pinabg.jwtvalidator.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.pinabg.jwtvalidator.service.JWTValidatorService;
 
 @RestController
 @RequestMapping("/main")
 public class JWTValidatorController {
+	
+	@Autowired
+	private JWTValidatorService jwtValidatorService;
+	
 	@RequestMapping("/greetings")
 	public String getGreetings() {
 		return "Hello World!";
---
+++ src/main/java/br/com/pinabg/jwtvalidator/service/JWTValidatorService.java
@@ -0,0 +1,8 @@
+package br.com.pinabg.jwtvalidator.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JWTValidatorService {
+
+}
