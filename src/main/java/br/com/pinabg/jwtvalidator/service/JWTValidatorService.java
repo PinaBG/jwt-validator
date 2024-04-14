354d77eacf7384fdf922b81caa1baed09ed5befc
PinaBG <brunogabrielpina@gmail.com>
2024-04-14 17:42:18 -0300

    feat: implementa metodo para verificar se String é nula ou vazia

--- src/main/java/br/com/pinabg/jwtvalidator/service/JWTValidatorService.java
+++ src/main/java/br/com/pinabg/jwtvalidator/service/JWTValidatorService.java
@@ -4,5 +4,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class JWTValidatorService {
-
+	public boolean stringIsNotNullOrEmpty(String name) {
+	    return !(name == null || name.isBlank());
+	}
 }
