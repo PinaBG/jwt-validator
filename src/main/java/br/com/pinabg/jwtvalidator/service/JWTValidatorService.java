d2ff1c3c5e0ff3641a4adb75800272a919646153
PinaBG <brunogabrielpina@gmail.com>
2024-04-17 20:11:04 -0300

    fix: troca chave secreta que impedia jwt de ser lido

--- src/main/java/br/com/pinabg/jwtvalidator/service/JWTValidatorService.java
+++ src/main/java/br/com/pinabg/jwtvalidator/service/JWTValidatorService.java
@@ -3,27 +3,78 @@ package br.com.pinabg.jwtvalidator.service;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import javax.crypto.SecretKey;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import br.com.pinabg.jwtvalidator.model.JwtResponseModel;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
-import io.jsonwebtoken.security.Password;
 
 @Service
 public class JWTValidatorService {
 	@Value("${jwt.secret}")
     private String jwtKey;
 	
-	public boolean validateJWT(String jwsToken) {
-		try {
-	        Claims claims = getJwtClaims(jwsToken);
-	        return jwtContainsMoreOrLessThan3Claims(claims) && jwtContainsNecessaryClaims(claims);
-	    } catch (JwtException e) {
-	        return false;
+	public JwtResponseModel loadAllValidations(String jwsToken) {
+	    JwtResponseModel jwtResponse = new JwtResponseModel();
+	    jwtResponse.setValidity("Verdadeiro");
+	    jwtResponse.setDescription("Justificativa: Abrindo o JWT, as informações contidas atendem a descrição:");
+
+	    Claims claims = validateJwt(jwsToken, jwtResponse);
+
+	    if (claims != null) {
+	        String payload = claims.toString();
+	        jwtResponse.setJwtPayload(payload);
+
+	        if (!jwtContainsMoreOrLessThan3Claims(claims)) {
+	            updateResponseForInvalid(jwtResponse, "mais/menos de 3 claims");
+	        } else if (!jwtContainsNecessaryClaims(claims)) {
+	            updateResponseForInvalid(jwtResponse, "não foram encontradas todas as claims necessárias");
+	        } else if (!stringIsNotNullOrEmpty(claims.get("Name", String.class))) {
+	            updateResponseForInvalid(jwtResponse, "a Claim Name é vazia");
+	        } else if (!nameDontContainNumbers(claims.get("Name", String.class))) {
+	            updateResponseForInvalid(jwtResponse, "a Claim Name possui caracter de números");
+	        } else if (!nameLenghtLessThan256Characters(claims.get("Name", String.class))) {
+	            updateResponseForInvalid(jwtResponse, "a Claim Name possui mais de 256 caracteres");
+	        } else if (!stringIsNotNullOrEmpty(claims.get("Role", String.class))) {
+	            updateResponseForInvalid(jwtResponse, "a Claim Role é vazia");
+	        } else if (!roleHasRightFormat(claims.get("Role", String.class))) {
+	            updateResponseForInvalid(jwtResponse, "a Claim Role não possui um dos tres valores ((Admin, Member e External)");
+	        } else if (!validateSeed(Integer.parseInt(claims.get("Seed", String.class)))) {
+	            updateResponseForInvalid(jwtResponse, "a Claim Seed não é um número primo");
+	        }
 	    }
+
+	    return jwtResponse;
+	}
+	
+	private void updateResponseForInvalid(JwtResponseModel jwtResponse, String justification) {
+	    jwtResponse.setValidity("Falso");
+	    jwtResponse.setDescription("Justificativa: Abrindo o JWT, " + justification + ".");
+	}
+
+	private Claims validateJwt(String jwsToken, JwtResponseModel jwtResponse) {
+		try {
+			return getJwtClaims(jwsToken);
+	    } catch (JwtException e) {
+	    	jwtResponse.setValidity("Falso");
+	    	jwtResponse.setDescription("Justificativa: JWT inválido");
+	    }
+		return null;
+	}
+	
+	public Claims getJwtClaims(String jwsToken) throws JwtException {
+		SecretKey key = Keys.hmacShaKeyFor(jwtKey.getBytes());
+		
+		return Jwts.parser()
+		        .verifyWith(key)
+		        .build()
+		        .parseSignedClaims(jwsToken)
+		        .getPayload();
 	}
 	
 	public boolean jwtContainsMoreOrLessThan3Claims(Claims claims) {
@@ -40,16 +91,6 @@ public class JWTValidatorService {
 			isValid = false;
 		}
 		return isValid;
-	}
-
-	public Claims getJwtClaims(String jwsToken) throws JwtException {
-		Password key = Keys.password(jwtKey.toCharArray());
-		
-		return Jwts.parser()
-		        .verifyWith(key)
-		        .build()
-		        .parseSignedClaims(jwsToken)
-		        .getPayload();
 	}
 	
 	public boolean validateName(String name) {
