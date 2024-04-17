4b1a8e484750496e58ffa775025225e4ffd52343
PinaBG <brunogabrielpina@gmail.com>
2024-04-17 20:09:55 -0300

    feat: cria endpoint para validador

--- src/main/java/br/com/pinabg/jwtvalidator/controller/JWTValidatorController.java
+++ src/main/java/br/com/pinabg/jwtvalidator/controller/JWTValidatorController.java
@@ -1,9 +1,13 @@
 package br.com.pinabg.jwtvalidator.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.pinabg.jwtvalidator.model.JwtResponseModel;
 import br.com.pinabg.jwtvalidator.service.JWTValidatorService;
 
 @RestController
@@ -16,5 +20,13 @@ public class JWTValidatorController {
 	@RequestMapping("/greetings")
 	public String getGreetings() {
 		return "Hello World!";
+	}
+	
+	@GetMapping("/validajwt")
+	public ResponseEntity<JwtResponseModel> validaJwt(@RequestParam String jwt) {
+		jwtValidatorService.loadAllValidations(jwt);
+		JwtResponseModel jwtResponse = jwtValidatorService.loadAllValidations(jwt);
+		
+		return ResponseEntity.ok(jwtResponse);
 	}
 }
